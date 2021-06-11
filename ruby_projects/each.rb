ec65c2c1787f0599ba0c92d8f2cf6732c0a83bdf
EC2 Default User <ec2-user@ip-172-31-36-90.ec2.internal>
2021-06-11 11:51:11 +0000

    day_end

---
+++ ruby_projects/each.rb
@@ -0,0 +1,14 @@
+numbers = [1, 2, 3, 4, 5]
+
+numbers.each do |number|
+    puts number
+end
+
+colors = ['red', 'green', 'blue']
+colors.each do |color|
+    puts color
+end
+
+for number in numbers do
+    puts number
+end
